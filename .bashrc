0bdde5235d01bd5d8b5969aea9bcde9b2a031330
Joseph George <joseph.fisat@gmail.com>
2017-08-24 16:33:04 +1200

    created login and register forms

---
+++ .bashrc
@@ -0,0 +1,1 @@
+cd C:\xampp\htdocs\ntec-repo
---
+++ assign2/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>My Assignment 1</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/customdiv.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <?php include("includes/nav.html"); ?>
+
+</div>
+    <section>
+    <div class="container customtopmargin">
+      <div class="row">
+          <div class="col-md-12 row_bottom_buffer"><div class="customdiv" id="map"></div>
+          </div>
+      </div>
+
+    </div>
+</section>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+  <?php include("includes/footer.html"); ?>
+</html>
